f1b03c8b853afafd2b57df181673976fb2c1c6e7
hrrs01 <haavard.nygaard@motiontech.no>
2026-01-05 10:04:49 +0100

    Transfer ctf shell

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "A nix flake containing hrrs01's home config";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    pwndbg.url = "github:pwndbg/pwndbg";
+    nixpkgs_unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs-25-11.url = "github:NixOS/nixpkgs/nixos-25.11";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+    snowfall-lib = {
+      url = "github:snowfallorg/lib";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs:
+    inputs.snowfall-lib.mkFlake {
+      inherit inputs;
+      src = ./.;
+    };
+
+}
